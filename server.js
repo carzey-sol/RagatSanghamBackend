af5250aa3222d8087e847c42cabb4dbc09f2d5d5
Mukesh Bajgain <112241933+carzey-sol@users.noreply.github.com>
2025-01-10 15:21:15 +0545

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(cors({
   origin: '*',  // Specify your frontend URL here
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
